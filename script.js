08a164023b2fc086bae62f50b79cfbbd1b5f0c2b
Prashant Salve <128687203+prashant04071995@users.noreply.github.com>
2024-04-25 08:01:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.getElementById('enterBtn').onclick = function () {
+  const statusParagraph = document.getElementById('status');
+  statusParagraph.innerHTML = '<h1>Entered Metaverse</h1>';
+};
